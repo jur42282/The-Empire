a0767bffea9d551736ce9477b90a89daf5fefbff
Lukas Jurak <jur42282@spseol.cz>
2025-02-28 12:04:39 +0100

    quest template

---
+++ quests_template.py
@@ -0,0 +1,12 @@
+class Quest:
+    def __init__(self, name:str, description:str, options:list):
+        self.name = name
+        self.description = description
+        self.options = options
+
+    def __str__(self):
+        return f"{self.name}\n{self.description}"
+    
+quest1 = Quest("quest1", "Testovací quest", [
+    {"answer_desc" : "Zaplať 500G", "yes" : "<funkce1>", "no": "<funkce2>"}
+])
