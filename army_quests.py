cfcb00a7ec1835042bb5afdabffc77bf9274ccae
Lukas Jurak <jur42282@spseol.cz>
2025-03-05 13:41:47 +0100

    army content

--- army_quests.py
+++ army_quests.py
@@ -1,3 +1,5 @@
+import main as m
+
 class Quest: # Záladní class, neměnit
     def __init__(self, name:str, description:str, options:list):
         self.name = name
@@ -7,9 +9,9 @@ class Quest: # Záladní class, neměnit
     def __str__(self):
         return f"{self.name}\n{self.description}"
     
-# zakládání questu    
-quest1 = Quest("quest1", "Testovací quest", [
-    {"answer_desc" : "Zaplať 500G", "yes" : "<funkce1>", "no": "<funkce2>"}
+# zakládání questu
+quest1 = Quest("Scammer", "Your Majesty, I beg you, to send thy guards to the city market. A vile thief plagues the market, and none dare stop him! Justice is needed. ", [
+    {"answer_desc" : "Will you send guards?", "They have greater matters to attend." : "<funkce1>", "Justice shall be served...": "<funkce2>"}
 ])
 
 # Storování questů template
@@ -21,3 +23,9 @@ quests = {
     Quest()
   ],
 }
+
+char = "PanzerKommandantLukas"
+
+def attribute_change(army=0, happiness=0, money=0, population=0, diplomacy1=0, diplomacy2=0, magic=0):
+    m.army += army
+    
--- quest_selector.py
+++ quest_selector.py
@@ -1,4 +1,5 @@
 from random import choice
+import army_quests
 def random_quest():
     return choice(quests)
 
