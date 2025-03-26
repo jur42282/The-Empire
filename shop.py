3b10a291d97819f0cdb6ced8b28784b184cdd3ca
Matyáš Mackovík <mac42396@spseol.cz>
2025-03-26 13:59:49 +0100

    navařil jsem shop

--- shop.py
+++ shop.py
@@ -1,4 +1,10 @@
 import random
+import json
+
+def change_stat(stat, value): #stat - string název statu, value - hodnota změny (číslo)
+    #načte soubor data.json (neměnit)
+    with open("data.json", "r", encoding="utf-8") as f: 
+        data = json.load(f)
 
 class ItemManager():
     def __init__(self, name : str, desc : str, price : int, can_sell = False) -> None:
@@ -17,27 +23,58 @@ class ShopManager():
         pass
 
     shop = [
-        ItemManager("Fontána","Postav fontánu a zvyš happiness obyvatelstva o 10%",1000),
-        ItemManager("Fontána","Postav fontánu a zvyš happiness obyvatelstva o 40%",1000),
-        ItemManager("Fontána","Postav fontánu a zvyš happiness obyvatelstva o 50%",1000)
+        ItemManager("Choice n.1", "c1", 1000),
+        ItemManager("Choice n.2", "c2", 2000),
+        ItemManager("Choice n.3", "c3", 3000)
             ]
 
+
+
     def open_shop():
-        pocitadlo = 0
         for item in ShopManager.shop:
-            pocitadlo += 1
-            print(f" [{pocitadlo}] {item}")
+    
+            print(f"{item}")
     
     def choice():
-        user_choice = input("Choose your option: ")
-        if user_choice in range(0, len(ShopManager.shop)):
-            item = ShopManager.shop[user_choice -1]
-            print("Vybral jsi f{item.name}")
+        global user_choice_shop
+        user_choice_shop = input("Choose your option: ")
+        if int(user_choice_shop) in range(0, len(ShopManager.shop) + 1):
+            user_choice_real = int(user_choice_shop) - 1
+            item = ShopManager.shop[user_choice_real]
+            print(f"You have selected {item.name} for {item.price} golds!")
+            print("Do you want to buy this item?")
+
+            #opatřit inputy aby necrashovalo
+            while True:
+                global final_choice_shop
+                amount = input("How much of this item you want to buy? (write number - or if you want to go back write 0): ")
+                
+                if amount.isnumeric():
+                    amount = int(amount)
+                    if amount > 0:
+                        print(f"You want to buy {amount} of {item.name} for {item.price * amount} golds?")
+                        final_choice_shop = input("yes/no: ")
+                        break
+                    #PŘIDAT MOŽNOST "ZPÁTKY" DO SHOPU
+                    else:
+                        print("Please, enter a number!") # oveřit funkčnost
+                else:
+                    print("Please, enter a number!")
+                
+
+
+            if final_choice_shop == "yes":
+                print(f"You have bought {item.name}!")
+                change_stat("money", (-item.price * amount)) # oveřit funkčnost
+
+            else:
+                print("You didn't buy the item!")
         else:
-            print("kkt")
+            print("upsík")
             print(len(ShopManager.shop))
 
         
+
 
 
 
@@ -46,6 +83,8 @@ def call_shop():
     shop_entity.open_shop()
     shop_entity.choice()
 
+# for i in(ShopManager.shop):
+#     print(i)
 
 call_shop()
 
